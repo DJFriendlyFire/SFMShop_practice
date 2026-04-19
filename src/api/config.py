222625bfba6c3279d19c117ed8917647a5b04ebe
DJFriendlyFire <melnikovich2014@gmail.com>
2026-04-19 17:36:36 +0500

    fixed ci.yml file х5

--- src/api/config.py
+++ src/api/config.py
@@ -6,10 +6,12 @@ load_dotenv()
 
 
 class Settings:
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "secret_key_something")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-    REFRESH_TOKEN_EXPIRE_DAYS: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS"))
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
+        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30")
+    )
+    REFRESH_TOKEN_EXPIRE_DAYS: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", "7"))
 
 
 settings = Settings()
